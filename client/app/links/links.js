df6f3ef883716f49ba68993bfd5b46534667c2ea
jae-shin <jae5464@gmail.com>
2016-07-08 15:26:45 -0700

    Implement links controller to pass tests

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,4 +2,11 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   // Your code here
+  // $scope.data = Links.getAll();
+  // or maybe
+  $scope.data = {};
+
+  Links.getAll().then(function(links) {
+    $scope.data.links = links;
+  });
 });
